80359725495ebe6e347911870500d8cb3be28083
unknown <acamalraj@gmail.com>
2021-05-26 23:01:32 +0530

    add ability create employee payroll object on save

--- js/newEmployeePayroll.js
+++ js/newEmployeePayroll.js
@@ -33,31 +33,14 @@ window.addEventListener('DOMContentLoaded',(event)=>{
     });
 
 });
+
 const save=()=>{
     try{
-        let employeePayrollData=createEmployeePayroll();
-        createAndUpdateStorage(employeePayrollData);
+        let EmployeePayrollData=createEmployeePayroll();
     }catch(e){
         return;
     }
 }
-function createAndUpdateStorage(employeePayrollData){
-    let employeePayrollList=JSON.parse(localStorage.getItem("EmployeePayrollList"));
-    if(employeePayrollList!=undefined){
-        employeePayrollList.push(employeePayrollData);
-    }else{
-        employeePayrollList=[employeePayrollData]
-    }
-    alert(employeePayrollList.toString());
-    localStorage.setItem("EmployeePayrollList",JSON.stringify(employeePayrollList));
-}
-// const save=()=>{
-//     try{
-//         let EmployeePayrollData=createEmployeePayroll();
-//     }catch(e){
-//         return;
-//     }
-// }
 const createEmployeePayroll=()=>{
     let employeePayrollData=new EmployeePayrollData();
     try{
