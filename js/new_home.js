19e807addd97353a6481c63fb0183b880f6f8964
unknown <acamalraj@gmail.com>
2021-06-03 13:27:53 +0530

    add: clent server program

---
+++ js/new_home.js
@@ -0,0 +1,97 @@
+ let empPayrollList;
+
+const getEmployeePayrollDataFromServer=()=>{
+    makeServiceCall("GET",site_properties.server_url,true).then(responseText=>{
+        empPayrollList=JSON.parse(responseText);
+        processEmployeePayrollDataResponse();
+    })
+    .catch(error=>{
+        console.log("GET error status"+JSON.stringify(error));
+        empPayrollList=[];
+        processEmployeePayrollDataResponse();
+    });
+}
+
+window.addEventListener('DOMContentLoaded',(event)=>{
+    if(site_properties.use_local_storage.match("true")){
+        getEmployeePayrollDataFromStorage();
+    }else getEmployeePayrollDataFromServer();
+});
+
+
+window.addEventListener('DOMContentLoaded',(event)=>{
+    empPayrollList=getEmployeePayrollDataFromStorage();
+    document.querySelector(".emp-count").textContent=empPayrollList.length;
+    createInnerHtml();
+    localStorage.removeItem('editEmp');
+});
+const getEmployeePayrollDataFromStorage=()=>{
+    
+    empPayrollList=localStorage.getItem('EmployeePayrollList')?
+            JSON.parse(localStorage.getItem('EmployeePayrollList')):[];
+            processEmployeePayrollDataResponse();
+}
+const processEmployeePayrollDataResponse=()=>{
+    document.querySelector(".emp-count").textContent=empPayrollList.length;
+    createInnerHtml();
+    localStorage.removeItem('editEmp');
+}
+const createInnerHtml=()=>{
+    const headerHtml="<th></th><th>Name</th><th>Gender</th><th>Department</th>"+
+    "<th>Salary</th><th>StartDate</th><th>Actions</th>";
+    if(empPayrollList.length==0) return;
+    let innerHtml=`${headerHtml}`;
+    for (const empPayrollData of empPayrollList){
+        innerHtml=`${innerHtml}
+        <tr>
+        <td><img class="profile" src="${empPayrollData._profilePic}" alt=""></td>
+        <td>${empPayrollData._name}</td>
+        <td>${empPayrollData._gender}</td>
+        <td><div class="dept-label">${getDeptHtml(empPayrollData._department)}</div></td>
+        <td>${empPayrollData._salary}</td>
+        <td>${stringifyDate(empPayrollData._startDate)}</td>
+        <td>
+        <img class="image" id="${empPayrollData.id}" onclick="remove(this)"
+                src="images/delete.png" alt="delete">
+        <img class="image" id="${empPayrollData.id}" onclick="update(this)"
+                src="images/edit.png" alt="edit">       
+        </td>        
+        
+        </tr>`;
+    }
+    document.querySelector('#table-display').innerHTML=innerHtml;
+    
+}
+const getDeptHtml=(deptList)=>{
+    let deptHtml='';
+    for(const dept of deptList){
+        deptHtml=`${deptHtml}<div class="dept-label">${dept}</div>`
+    }
+    return deptHtml;
+}
+const remove=(node)=>{
+    let empPayrollData=empPayrollList.find(empData=>empData.id==node.id)
+    if(!empPayrollData) return;
+    const index=empPayrollList.map(empData=>empData.id).indexOf(empPayrollData.id);
+    empPayrollList.splice(index,1);
+    if(site_properties.use_local_storage.match("true")){
+        localStorage.setItem("EmployeePayrollList",JSON.stringify(empPayrollList));
+        createInnerHtml();
+    }else{
+        const deleteURL=site_properties.server_url+empPayrollData.id.toString();
+        makeServiceCall("DELETE",deleteURL,false)
+        .then(responseText=>{
+            createInnerHtml();
+        })
+        .catch(error=>{
+            console.log("DELETE Error Status:"+JSON.stringify(error));
+        });
+    }
+
+}
+const update=(node)=>{
+    let empPayrollData=empPayrollList.find(empData=>empData.id==node.id)
+    if(!empPayrollData)return;
+    localStorage.setItem('editEmp',JSON.stringify(empPayrollData))
+    window.location.replace(site_properties.add_emp_payroll_page);
+}
---
+++ js/siteProperties.js
@@ -0,0 +1,6 @@
+let site_properties={
+    use_local_storage:"false",
+    home_page:"homePage.html",
+    add_emp_payroll_page:"employeePayroll.html",
+    server_url:"http://127.0.0.1:3000/EmployeePayrollDB/"
+};
